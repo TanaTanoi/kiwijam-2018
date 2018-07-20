11ea87eee079dd314968965ec1a3fc283fb5d9da
TanaTanoi <tana.tanoi@gmail.com>
2018-07-21 10:14:50 +1200

    Change simple movement to keyboard controls

--- Assets/scripts/CharacterInput.cs
+++ Assets/scripts/CharacterInput.cs
@@ -13,13 +13,18 @@ public class CharacterInput : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		RaycastHit hit;
-        Ray ray = this.camera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit)) {
-            Vector3 hitPos = hit.point;
-			Vector3 direction = hitPos - controller.transform.position;
-			direction.Normalize();
-			this.controller.SimpleMove(direction);
-        }
+		// stuff for mouse direction
+		// RaycastHit hit;
+        // Ray ray = this.camera.ScreenPointToRay(Input.mousePosition);
+        // if (Physics.Raycast(ray, out hit)) {
+        //     Vector3 hitPos = hit.point;
+		// 	Vector3 direction = hitPos - controller.transform.position;
+		// 	direction.Normalize();
+        // }
+
+		float vertical = Input.GetAxis("Vertical");
+		float horizontal = Input.GetAxis("Horizontal");
+		Vector3 direction = new Vector3(-vertical, 0, horizontal);
+		this.controller.SimpleMove(direction.normalized * 10);
 	}
 }
