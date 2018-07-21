c5b81b21ae21d500aedb1378ed5d41f63b61185f
darrenhobern <darren.hobern@gmail.com>
2018-07-21 12:28:17 +1200

    Update EnemySpawner to spawn at a random point in the transform's scale
    Therefore only allows for rectangular spawn regions.

--- Assets/scripts/EnemySpawner.cs
+++ Assets/scripts/EnemySpawner.cs
@@ -8,6 +8,8 @@ public class EnemySpawner : MonoBehaviour {
     [SerializeField] private float spawnInterval = 1f;
     [SerializeField] private float intervalWiggle = 0.2f;
     [SerializeField] private bool playing = true; // TODO move this to gamecontroller
+    [SerializeField] private Transform spawnRegion;
+
 
 	// Update is called once per frame
 	void Start () {
@@ -16,11 +18,14 @@ public class EnemySpawner : MonoBehaviour {
 
     public IEnumerator SpawnEnemy() {
         while(playing) {
-            // TODO spawn in region
-            Instantiate(enemyPrefab, Vector3.zero, Quaternion.identity, transform);
+            Vector3 spawnPosition = new Vector3(spawnRegion.position.x, spawnRegion.position.y, spawnRegion.position.z);
+            spawnPosition.x += Random.Range(-(spawnRegion.lossyScale.x/2), spawnRegion.lossyScale.x/2);
+            spawnPosition.y += Random.Range(-(spawnRegion.lossyScale.y/2), spawnRegion.lossyScale.y/2);
+            spawnPosition.z += Random.Range(-(spawnRegion.lossyScale.z/2), spawnRegion.lossyScale.z/2
+
+            Instantiate(enemyPrefab, spawnPosition, Quaternion.identity, transform);
 
             float delay = spawnInterval + Random.Range(-intervalWiggle, intervalWiggle);
-            print(delay);
             yield return new WaitForSeconds(delay);
         }
     }
