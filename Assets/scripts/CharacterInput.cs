9737669c41cf73a9242711bd7bc4ce3baf6024c0
CDeighton <cullumdeighton@gmail.com>
2018-07-21 16:02:58 +1200

    Added AOE damage from shell casings and fixed satanic movement issues

--- Assets/scripts/CharacterInput.cs
+++ Assets/scripts/CharacterInput.cs
@@ -5,28 +5,48 @@ using UnityEngine;
 public class CharacterInput : MonoBehaviour {
 	public Camera camera;
 
-	// private Rigidbody rb;
-	private const float speed = 5;
+	private const float SPEED = 5;
+	private const int MAX_HEALTH = 100;
 
-	// void Start() {
-	// 	rb = GetComponent<Rigidbody>();
-	// }
+	private double regenRate = 1;
+	private double health;
+
+
+	void Start() {
+		health = MAX_HEALTH;
+	}
 
 	void Update() {
+		ApplyRegen();
 		FaceMouse();
+		MovePlayer();
+	}
 
-		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	void OnTriggerStay(Collider other) { health -= 0.5 * Time.deltaTime; }
 
-		transform.Translate(movement * speed * Time.deltaTime);
+	private void ApplyRegen() {
+		health += regenRate * Time.deltaTime;
+		if (health > MAX_HEALTH) health = MAX_HEALTH;
 	}
 
 	private void FaceMouse() {
 		RaycastHit hit;
 		Ray ray = this.camera.ScreenPointToRay(Input.mousePosition);
+
 		if (Physics.Raycast(ray, out hit)) {
 			Vector3 hitPos = hit.point;
 
 			transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
 		}
+	}
+
+	private void MovePlayer() {
+		Vector3 movement = new Vector3(
+			-Input.GetAxis("Vertical"),
+			0,
+			Input.GetAxis("Horizontal")
+		);
+
+		transform.position += movement * SPEED * Time.deltaTime;
 	}
 }
