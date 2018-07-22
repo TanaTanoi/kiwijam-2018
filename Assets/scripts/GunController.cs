0e186a19a1f9fc409e2ac2fc19736d1345d7b7dc
TanaTanoi <tana.tanoi@gmail.com>
2018-07-22 12:09:39 +1200

    Add laser sound effect

--- Assets/scripts/GunController.cs
+++ Assets/scripts/GunController.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour {
 	private const string FIRE_KEY = "space";
+	public AudioSource laserSound;
+	public const float SHOOT_COOLDOWN = 0.5f;
 
 	public ParticleSystem shot;
 	public Transform bullet;
+	TimeSince timeSinceLastShot;
+
+	void Start() {
+		this.laserSound = this.laserSound ?? this.GetComponent<AudioSource>();
+	}
 
 	void Update () {
-		if (Input.GetKeyDown(FIRE_KEY)) {
+		if (this.timeSinceLastShot > SHOOT_COOLDOWN && Input.GetKeyDown(FIRE_KEY)) {
+			this.timeSinceLastShot = 0;
 			Transform clone = Instantiate(bullet, transform.position, Quaternion.identity);
+			this.laserSound.pitch = UnityEngine.Random.Range(2f, 2.5f);
+			this.laserSound.Play();
 
 			shot.Play();
 		}
